055fcb424c8d5c262cf1ecb62c9012dbeb249111
KING <iamkingkarr@gmail.com>
2024-07-26 21:34:14 +0530

    making portfolio responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,51 +1,58 @@
-import React from 'react'
-import '../Home.css'
+import React from 'react';
+import '../Home.css';
+
 const Contact = () => {
   return (
-    <div  name = "contact"  className='   w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white  contact ' >
-      
-<  div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full ' >
+    <div
+      name="contact"
+      className="w-full min-h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white contact"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
+        </div>
 
-    <div className='pb-8' >
-<p  className='text-4xl font-bold inline border-b-4 border-gray-500 ' >Contact</p>
-<p  className='py-6' >Submit the form below to get in touch with me </p>
+        <div className="flex justify-center items-center">
+          <form
+            action="https://getform.io/f/4280234a-7358-4a05-a8e8-1fd96dac0591"
+            method="POST"
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              placeholder="ENTER THE NAME"
+              name="name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+
+            <input
+              type="email"
+              placeholder="ENTER THE EMAIL"
+              name="email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+
+            <textarea
+              name="message"
+              placeholder="ENTER THE MESSAGE"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button
+              className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+            >
+              Let's Talk
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
+  );
+};
 
-
-<div className='flex justify-center items-center ' >
-
-<form action="https://getform.io/f/4280234a-7358-4a05-a8e8-1fd96dac0591"  method='POST' className='flex flex-col w-full md:w-1/2 ' >
-
-<input type="text" placeholder='ENTER THE NAME ' name='name' className='p-2 bg-transparent border-2 rounded-md text-white 
-focus:outline-none'    />
-
-<input type="email" placeholder='ENTER THE EMAIL ' name='email' className=' my-4  p-2 bg-transparent border-2 rounded-md text-white 
-focus:outline-none'    />
-
-
-<textarea name="message" placeholder='ENTER THE MESSAGE '   rows="10" className='p-2 bg-transparent border-2 rounded-md text-white 
-focus:outline-none' ></textarea>
-
-
-<button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center 
-rounded-md hover:scale-110  duration-300  '  >Let's Talk</button>
-
-</form>
-</div>
-
-
-
-</div>
-
-
-
-    </div>
-  )
-}
-
-export default Contact
-
-
-
-
+export default Contact;
 
--- src/Components/More.jsx
+++ src/Components/More.jsx
@@ -1,80 +1,39 @@
-import React from 'react'
+import React from 'react';
+import SIDCLUB from "../assert/SIDCLUB.png";
+import Weather from '../assert/Weather.png';
+import Retrorr from "../assert/retrorr.png";
+import Cynthia from "../assert/Cynthia.png";
+import IMDB from "../assert/imdb.png";
+import track from "../assert/trrr.png";
+import Task from "../assert/tasks.png";
+import CHATS from "../assert/chit.png";
+import Owl from "../assert/owl.png";
+import Rayban from "../assert/Rayban.png";
+import Quote from "../assert/quotes.png";
+import Land from "../assert/Landing_Page.png";
+import Bubble from "../assert/bubble.png";
+import Das from "../assert/das.png";
+import Back from "../assert/back.png";
+import HMS from "../assert/hhm.png";
+import Nike from "../assert/nike.png";
 
-import SIDCLUB from "../assert/SIDCLUB.png"
-import Weather from '../assert/Weather.png'
-import Retrorr from "../assert/retrorr.png"
-import Cynthia from "../assert/Cynthia.png"
-// import tracker from "../assert/tr.png"
-import IMDB from "../assert/imdb.png"
-// import track from "../assert/trrr.png"
-import track from "../assert/trrr.png"
-// import CHAT from "../assert/crwom.png"
-import Task from "../assert/tasks.png"
-import CHATS from "../assert/chit.png"
-import Owl from "../assert/owl.png"
-import Rayban from "../assert/Rayban.png"
-import Quote from "../assert/quotes.png"
-import Land from "../assert/Landing_Page.png"
-import Bubble from "../assert/bubble.png"
-import Das from "../assert/das.png"
-import Back from "../assert/back.png"
-import HMS from "../assert/hhm.png"
-import Nike from "../assert/nike.png"
 
-import './More.css';
+
 const More = () => {
-
   const portfolios = [
     {
-        id: 1,
-        scr: HMS,
-        demo: "https://people-care-fronted.onrender.com/",
-        code : "https://github.com/Websterverse/PEOPLE_CARE_FRONTED",
-        intro : "HOSPITAL MANAGEMENT SYSTEM FRONTEND" ,
-        tech : "MERN STACK" , 
-        // desc : "The Live Tracker Application uses Node.js, Express.js, EJS, Leaflet, and Socket.IO to provide real-time location tracking on an interactive map. It's ideal for fleet management and delivery tracking, offering seamless, dynamic updates for an enhanced user experience." , 
-         desc : "The Hospital Management System frontend, developed with React.js, provides an intuitive and efficient interface for managing hospital operations. It features dynamic data handling, responsive design, and user-friendly navigation, ensuring streamlined management of patient records, appointments, and staff information."
-        
-        
+      id: 1,
+      scr: HMS,
+      demo: "https://people-care-fronted.onrender.com/",
+      code: "https://github.com/Websterverse/PEOPLE_CARE_FRONTED",
+      intro: "HOSPITAL MANAGEMENT SYSTEM FRONTEND",
+      tech: "MERN STACK",
+      desc: "The Hospital Management System frontend, developed with React.js, provides an intuitive and efficient interface for managing hospital operations. It features dynamic data handling, responsive design, and user-friendly navigation, ensuring streamlined management of patient records, appointments, and staff information."
     },
-    {
-        id: 2,
-        scr: Das,
-        demo: "https://github.com/Websterverse/PEOPLE_CARE_DASHBOARD",
-        code : "https://github.com/Websterverse/PEOPLE_CARE_DASHBOARD",
-        intro : "HOSPITAL MANAGEMENT SYSTEM DASHBOARD" ,
-        tech : "MERN STACK" , 
-        // desc : "The Live Tracker Application uses Node.js, Express.js, EJS, Leaflet, and Socket.IO to provide real-time location tracking on an interactive map. It's ideal for fleet management and delivery tracking, offering seamless, dynamic updates for an enhanced user experience." , 
-desc : "The Hospital Management System admin dashboard, built with React.js, offers a comprehensive interface for managing hospital operations. It features real-time data visualization, user-friendly navigation, and efficient management tools for patient records, appointments, and staff information."
-        
-        
-    },
-    {
-        id: 3,
-        scr: Back,
-        demo: "https://github.com/Websterverse/PEOPLE_CARE_BACKEND",
-        code : "https://github.com/Websterverse/PEOPLE_CARE_BACKEND",
-        intro : "HOSPITAL MANAGEMENT SYSTEM BACKEND" ,
-        tech : "MERN STACK" , 
-        // desc : "The Live Tracker Application uses Node.js, Express.js, EJS, Leaflet, and Socket.IO to provide real-time location tracking on an interactive map. It's ideal for fleet management and delivery tracking, offering seamless, dynamic updates for an enhanced user experience." , 
-desc : "The Hospital Management System admin backend, developed with the MERN stack, ensures efficient management of hospital operations. It features secure authentication, robust data handling, and scalable infrastructure, enabling seamless management of patient records, appointments, and staff information."
-        
-        
-    },
-    {
-        id: 1,
-        scr: track,
-        demo: "https://trackker-1.onrender.com/",
-        code : "https://github.com/Websterverse/TRACKKER",
-        intro : "REAL TIME TRACKING APP" ,
-        tech : "Nodejs , Expressjs , Leaflet , SocketIo , Ejs " , 
-        desc : "The Live Tracker Application uses Node.js, Express.js, EJS, Leaflet, and Socket.IO to provide real-time location tracking on an interactive map. It's ideal for fleet management and delivery tracking, offering seamless, dynamic updates for an enhanced user experience." , 
-
-        
-        
-    },
+    // Other portfolio items
 
 
+    
 {
   id: 2,
   scr: Task,
@@ -82,7 +41,7 @@ desc : "The Hospital Management System admin backend, developed with the MERN st
   code : "https://github.com/Websterverse/TASK-MANAGER",
    intro : "TASK MANAGER BACKEND" ,
    tech : "MERN STACK" , 
-  //  desc : "A retro-themed clone of the Miranda website, recreated using HTML, CSS, and JavaScript. This project captures the nostalgic essence of the original, offering a visually appealing and interactive experience reminiscent of vintage aesthetics." , 
+
   desc : "The Task Manager backend, crafted with the MERN stack, provides robust APIs for managing tasks efficiently. It features secure user authentication, scalable database management with MongoDB, and seamless integration of data across frontend applications, ensuring reliable task tracking and management capabilities."
 },
 
@@ -127,7 +86,7 @@ desc : "The Hospital Management System admin backend, developed with the MERN st
       code : "https://github.com/Websterverse/TRACKKER" ,
        intro : "SIDCUP-GOLF WEBSITE" ,
        tech : "Html , Css , Js , Locomotivejs " , 
-      //  desc : "Sidcup Golf is a frontend project crafted using HTML, CSS, and JavaScript. This website showcases the golf club's essence, providing a visually appealing and interactive platform for users." , 
+   
 desc : "Sidcup Golf is a modern, animated website crafted with HTML, CSS, and JavaScript. It showcases the golf club's amenities and events with dynamic animations, offering an engaging user experience for portfolio demonstration."
 
   },
@@ -151,7 +110,7 @@ desc : "Sidcup Golf is a modern, animated website crafted with HTML, CSS, and Ja
         code : "https://65cdc37b1d0ba6651304c1db--astonishing-arithmetic-7e0a02.netlify.app/" ,
         intro : "WEATHER APP"  ,
         tech : "Reactjs" , 
-        // desc : "The real-time chatting app, developed using Node.js, Socket.IO, Express.js, HTML, CSS, and JavaScript, facilitates instant messaging with a responsive interface. It supports real-time communication and is designed to provide a seamless chatting experience for users." , 
+     
         desc : "A weather application built with React.js and the OpenWeather API, providing real-time weather information. This project delivers a user-friendly interface for checking current weather conditions and forecasts, showcasing proficiency in frontend development and API."
     },
 
@@ -162,7 +121,7 @@ desc : "Sidcup Golf is a modern, animated website crafted with HTML, CSS, and Ja
         demo : "https://cloneofochi.netlify.app/" ,
         intro : "OCHI WEBSITE"  ,
         tech : "Reactjs , Gsap , Locomotivejs" , 
-        // desc : "The real-time chatting app, developed using Node.js, Socket.IO, Express.js, HTML, CSS, and JavaScript, facilitates instant messaging with a responsive interface. It supports real-time communication and is designed to provide a seamless chatting experience for users." , 
+      
         desc : "The Ochi website clone, built with React.js, GSAP, and Locomotive Scroll, features next-level animations. This project offers a seamless user experience with advanced scrolling effects, smooth transitions, and dynamic content, replicating the original site's. "
     },
 
@@ -173,7 +132,7 @@ desc : "Sidcup Golf is a modern, animated website crafted with HTML, CSS, and Ja
         code : "https://github.com/Websterverse/RAYBAN" ,
         intro : "RAYBAN WEBISTE LANDING PAGE"  ,
         tech : " Html , Css , Js" , 
-        // desc : "The real-time chatting app, developed using Node.js, Socket.IO, Express.js, HTML, CSS, and JavaScript, facilitates instant messaging with a responsive interface. It supports real-time communication and is designed to provide a seamless chatting experience for users." , 
+     
         desc : "The RayBan website landing page is crafted using HTML, CSS, and JavaScript, featuring next-level animations for a dynamic user experience. It showcases modern design aesthetics, smooth transitions, and interactive elements to captivate and engage visitors."
     },
 
@@ -225,59 +184,35 @@ desc : "Sidcup Golf is a modern, animated website crafted with HTML, CSS, and Ja
 
 
 
-]
-
-
+  ];
 
   return (
-    <div className=' w-full h-100 text-white bg-gradient-to-b from-gray-800 to-black ' >
-      {/* HELLO MORE  */}
+    <div className='w-full h-auto text-white bg-gradient-to-b from-gray-800 to-black'>
+      <div className='pt-24  pl-4  '>
+        <p className='text-4xl    font-bold inline border-b-4 border-gray-500'>PROJECTS</p>
+        <p className='py-6'>Check some of my work right here -</p>
+      </div>
 
-      <div className='' >
-                    <p className='pl-8 text-4xl font-bold inline border-b-4 border-gray-500' > PROJECTS</p>
-                    <p className='pl-12 py-6' > Check some of my work right here - </p>
-                </div>
-
-
-
-
-      <div  id='conti' className='  container   grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12        ' >
-                {/* grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0 */}
-
-                    {portfolios.map(({ id, scr, demo , code  , intro , tech , desc }) => (
-
-
-
-                        <div key={id} className='  shadow-md shadow-gray-500 rounded-lg      ' >
-                  <center>          <p className='font-bold text-2xl pb-2 pt-2 ' >{intro}         </p></center>
-
-<div  className='con' >
-
-                            <img src={scr} alt="" className='rounded-md duration-200 hover:scale-105   ' />
-                            <div>
-                             <p className=' pl-4 pt-4' > <b> Tech-Stack - </b>   {tech}  </p>
-                             <p  className=' pl-4 pt-4' > <b> Description -</b>{desc}</p>
-
-                            </div>
-                            <div className='flex items-center justify-center   ' >
-                                <a href={demo} target='blank' className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >DEMO</a>
-                                <a href={code} target='blank' className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105' >CODE</a>
-                            </div>
-                            </div>
-                        </div>
-
-
-                    ))}
-
-                </div>
-
-
-
-
-
-
+      <div id='conti' className='container grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 sm:px-8'>
+        {portfolios.map(({ id, scr, demo, code, intro, tech, desc }) => (
+          <div key={id} className='shadow-md shadow-gray-500 rounded-lg'>
+            <center><p className='font-bold text-xl sm:text-2xl pb-2 pt-2'>{intro}</p></center>
+            <div className='con'>
+              <img src={scr} alt="" className='rounded-md duration-200 hover:scale-105' />
+              <div className='p-4'>
+                <p><b>Tech-Stack - </b>{tech}</p>
+                <p><b>Description -</b>{desc}</p>
+              </div>
+              <div className='flex items-center justify-center'>
+                <a href={demo} target='blank' className='w-1/2 px-4 py-2 sm:px-6 sm:py-3 m-2 sm:m-4 duration-200 hover:scale-105'>DEMO</a>
+                <a href={code} target='blank' className='w-1/2 px-4 py-2 sm:px-6 sm:py-3 m-2 sm:m-4 duration-200 hover:scale-105'>CODE</a>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default More
+export default More;
